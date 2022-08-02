02a778691a088f5f19e637d1acceba63bb860baf
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-08-02 12:36:12 +0100

    ...

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Main = () => {
+  return (
+    <>
+<section className=' h-screen'>
+  <div className=''>
+    <div className=' flex  justify-end'>
+      <button> &deg;C </button>
+      <button> &deg;F </button>
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default Main
